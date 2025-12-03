6fae2d05afd9b14914552497f67c9b212223960c
mussygee <cgoldste9@student.touro.edu>
2025-12-02 20:30:02 -0500

    Complete HW8 Collections: ExtendedLinkedCollection and BasicBag

--- src/main/java/exercises/BasicBag.java
+++ src/main/java/exercises/BasicBag.java
@@ -2,25 +2,65 @@ package exercises;
 
 import edu.touro.mcon264.apps.collections.ArrayCollection;
 import edu.touro.mcon264.apps.collections.BagInterface;
+import java.util.Random;
 
 public class BasicBag<T> extends ArrayCollection<T> implements BagInterface<T> {
+    private final Random rand = new Random();
     @Override
     public T grab() {
-        return null;
+        if (numElements == 0) {
+            return null;
+        }
+
+        int index = rand.nextInt(numElements);
+        T result = elements[index];
+
+        elements[index] = elements[numElements-1];
+        elements[numElements-1] = null;
+        numElements--;
+
+        return result;
     }
 
     @Override
     public int count(T target) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < numElements; i++) {
+            if (elements[i].equals(target)) {
+                count++;
+            }
+        }
+        return count;
     }
 
     @Override
     public int removeAll(T target) {
-        return 0;
+        int removed = 0;
+        int i = 0;
+
+        while (i < numElements) {
+            if (elements[i].equals(target)) {
+                // overwrite with last element
+                elements[i] = elements[numElements - 1];
+                elements[numElements - 1] = null;
+                numElements--;
+                removed++;
+                // do not advance i – we need to re-check the new element at i
+            } else {
+                i++;
+            }
+        }
+
+        return removed;
     }
 
     @Override
     public void clear() {
+        for (int i = 0; i < numElements; i++) {
+            elements[i] = null;
+        }
+        numElements = 0;
+    }
 
     }
 
@@ -28,6 +68,3 @@ public class BasicBag<T> extends ArrayCollection<T> implements BagInterface<T> {
     // consider extending ArrayCollection<T>
     // put in the JavaDoc comments as specified in the BagInterface.
     // Which SOLID principles is being applied here?
-
-
-}
--- src/main/java/exercises/ExtendedLinkedCollection.java
+++ src/main/java/exercises/ExtendedLinkedCollection.java
@@ -1,19 +1,69 @@
 package exercises;
 
 import edu.touro.mcon264.apps.collections.LinkedCollection;
+import edu.touro.mcon264.support.LLNode;
 
 public class ExtendedLinkedCollection<T> extends LinkedCollection<T> {
     @Override
     public String toString() {
       //ТОDO: implement this method. Use StringBuider. Create Javadoc.
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        LLNode<T> current = head;
+        boolean first = true;
+
+        while (current != null) {
+            if (!first) {
+                sb.append(", ");
+            }
+            sb.append(current.getInfo());
+            first = false;
+            current = current.getLink();
+        }
+
+        sb.append("]");
+        return sb.toString();
     }
 
 
     public int count(T target) {
       //ТОDO: implement this method. Count the number of times target appears in the collection. Create Javadoc.
+
+        int count = 0;
+        LLNode<T> current = head;
+        while (current != null) {
+            if (current.getInfo().equals(target)) {
+                count++;
+            }
+            current = current.getLink();
+        }
+        return count;
     }
 
     public void removeAll(T target) {
       //ТОDO: implement this method. Remove all occurrences of target from the collection and return the number of elements removed. Create Javadoc.
+
+        while (head != null && head.getInfo().equals(target)) {
+            head = head.getLink();
+            numElements--;
+        }
+
+        // now head is either null or not equal to target
+        LLNode<T> previous = head;
+        LLNode<T> current = (head == null) ? null : head.getLink();
+
+        while (current != null) {
+            if (current.getInfo().equals(target)) {
+                // bypass current
+                previous.setLink(current.getLink());
+                numElements--;
+                current = previous.getLink();
+            } else {
+                previous = current;
+                current = current.getLink();
+            }
+        }
     }
 }
